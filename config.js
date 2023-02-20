01ef0a4e3f703af7780b9693f0ca2b134aeb27e3
AndreyJRA <andreyrodriguez66.ar@gmail.com>
2023-02-19 22:35:23 -0600

    Revert "hola"
    This reverts commit b77744bda99700edba0a967cd83d5a45295d2823.

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'localhost',
+    host: env.DB_HOST || 'otto.db.elephantsql.com',
     port: env.DB_PORT || '5432',
-    user: env.DB_USER || 'apidesarrollo',
-    password: env.DB_PASSWORD || '123456',
-    database: env.DB_NAME || 'baseapi',
+    user: env.DB_USER || 'cklijfef',
+    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
+    database: env.DB_NAME || 'cklijfef',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
